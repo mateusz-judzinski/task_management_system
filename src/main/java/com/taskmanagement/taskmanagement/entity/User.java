ea117719e908893697bf6408dac4bf2c3bae1396
Mateusz Judziński <mateusz.judzinski@gmail.com>
2024-10-21 19:46:05 +0200

    lil somethin

---
+++ src/main/java/com/taskmanagement/taskmanagement/entity/User.java
@@ -0,0 +1,12 @@
+package com.taskmanagement.taskmanagement.entity;
+
+
+import java.util.List;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String email;
+
+}
