6a6598f4e5fc77267d4791ad3c08bcf51769f724
Mateusz Judziński <mateusz.judzinski@gmail.com>
2024-11-09 12:31:34 +0100

    starting with test for AdminService class

--- src/test/java/com/taskmanagement/taskmanagement/service/AdminServiceSpec.groovy
+++ src/test/java/com/taskmanagement/taskmanagement/service/AdminServiceSpec.groovy
@@ -1,6 +1,23 @@
 package com.taskmanagement.taskmanagement.service
 
+import com.taskmanagement.taskmanagement.entity.User
 import spock.lang.Specification
 
 class AdminServiceSpec extends Specification{
+
+    UserService userService = Mock(UserService)
+    TaskService taskService = Mock(TaskService)
+
+    AdminServiceImpl adminService
+
+    def setup(){
+        adminService = new AdminServiceImpl(userService, taskService)
+    }
+
+    def "should add user to by calling register method in UserService"(){
+        given: "a user to be add"
+        def user = new User(username: "testUser", email: "test@gmail.com")
+
+
+    }
 }
