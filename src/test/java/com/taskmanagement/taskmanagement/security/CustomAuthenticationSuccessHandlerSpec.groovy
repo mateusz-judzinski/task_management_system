162da1ead63036de10a4258172c8e914a4c9da4b
Mateusz Judziński <mateusz.judzinski@gmail.com>
2024-11-11 15:01:03 +0100

    added some test to security package but didnt finished

---
+++ src/test/java/com/taskmanagement/taskmanagement/security/CustomAuthenticationSuccessHandlerSpec.groovy
@@ -0,0 +1,63 @@
+package com.taskmanagement.taskmanagement.security
+
+import com.taskmanagement.taskmanagement.security.CustomAuthenticationSuccessHandler
+import spock.lang.Specification
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+class CustomAuthenticationSuccessHandlerSpec extends Specification {
+
+    def "onAuthenticationSuccess should redirect to /tasks for ROLE_USER"() {
+        given: "CustomAuthenticationSuccessHandler, mock request, response, and authentication with USER role"
+        def handler = new CustomAuthenticationSuccessHandler()
+        def request = Mock(HttpServletRequest)
+        def response = Mock(HttpServletResponse)
+        def authentication = Mock(Authentication)
+
+        def authorities = [new SimpleGrantedAuthority("ROLE_USER")]
+        authentication.getAuthorities() >> authorities
+
+        when: "onAuthenticationSuccess is called"
+        handler.onAuthenticationSuccess(request, response, authentication)
+
+        then: "response should redirect to '/tasks'"
+        1 * response.sendRedirect("/tasks")
+    }
+
+    def "onAuthenticationSuccess should redirect to /admin for ROLE_ADMIN"() {
+        given: "CustomAuthenticationSuccessHandler, mock request, response, and authentication with ADMIN role"
+        def handler = new CustomAuthenticationSuccessHandler()
+        def request = Mock(HttpServletRequest)
+        def response = Mock(HttpServletResponse)
+        def authentication = Mock(Authentication)
+
+        def authorities = [new SimpleGrantedAuthority("ROLE_ADMIN")]
+        authentication.getAuthorities() >> authorities
+
+        when: "onAuthenticationSuccess is called"
+        handler.onAuthenticationSuccess(request, response, authentication)
+
+        then: "response should redirect to '/admin'"
+        1 * response.sendRedirect("/admin")
+    }
+
+    def "onAuthenticationSuccess should redirect to / for unknown roles"() {
+        given: "CustomAuthenticationSuccessHandler, mock request, response, and authentication with an unknown role"
+        def handler = new CustomAuthenticationSuccessHandler()
+        def request = Mock(HttpServletRequest)
+        def response = Mock(HttpServletResponse)
+        def authentication = Mock(Authentication)
+
+        def authorities = [new SimpleGrantedAuthority("ROLE_UNKNOWN")]
+        authentication.getAuthorities() >> authorities
+
+        when: "onAuthenticationSuccess is called"
+        handler.onAuthenticationSuccess(request, response, authentication)
+
+        then: "response should redirect to '/'"
+        1 * response.sendRedirect("/")
+    }
+}
--- src/test/java/com/taskmanagement/taskmanagement/security/SecurityConfigSpec.groovy
+++ src/test/java/com/taskmanagement/taskmanagement/security/SecurityConfigSpec.groovy
@@ -1,6 +1,10 @@
 package com.taskmanagement.taskmanagement.security
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
 import spock.lang.Specification
 
-class SecurityConfigSpec extends Specification{
+// in progress, need some research
+class SecurityConfigSpec extends Specification {
+
 }
